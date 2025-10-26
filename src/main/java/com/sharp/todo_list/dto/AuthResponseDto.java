d6865de5952f856f196d4b86b9c58bce61a37070
Hugo <hugo.encaminhados@gmail.com>
2025-10-26 15:43:28 -0300

    Changing auth response dto

--- src/main/java/com/sharp/todo_list/dto/AuthResponseDto.java
+++ src/main/java/com/sharp/todo_list/dto/AuthResponseDto.java
@@ -1,5 +1,6 @@
 package com.sharp.todo_list.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,8 +8,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LoginResponseDto {
 
+    private String name;
+    private String email;
     private String token;
     private long expiresIn;
 }
